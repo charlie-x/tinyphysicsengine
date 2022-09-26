d8ecf78bbcdf61b753138f84b3f26093f60159fc
Miloslav Ciz <drummyfish@disroot.org>
2022-09-26 20:33:54 +0200

    Remove script

--- make.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-PROGRAM=test_sdl
-
-rm $PROGRAM
-g++ -x c -std=c99 -g -Wall -Wextra -pedantic -O3 -Wall -Wextra -Wno-unused-parameter -Wno-missing-field-initializers -lSDL2 -o $PROGRAM $PROGRAM.c && ./$PROGRAM
